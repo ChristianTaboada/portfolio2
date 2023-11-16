65df3e049a8c39805c872bb6df3babbd7897dd90
ChristianTaboada <taboada.christian@gmail.com>
2023-11-16 03:22:43 -0300

    technologies y pdf listo

--- src/components/info/skills/Technologies.tsx
+++ src/components/info/skills/Technologies.tsx
@@ -2,7 +2,98 @@ import React from 'react'
 
 const Technologies: React.FC = (): JSX.Element => {
   return (
-        <h1>hola</h1>
+        <div className='flex flex-col mx-10'>
+          <div className='flex items-center mt-5'>
+            <span className='text-gray-400 mr-2'>H2</span>
+            <h2 className='text-2xl'>Tecnologias</h2>
+          </div>
+          <div className='ml-8 mt-4'>
+            <h3>Lenguajes y Tools</h3>
+            <div className='flex mt-2 space-x-2'>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/javascript.png' alt='javascript' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>JavaScript</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/java.png' alt='java' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>Java</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/python.png' alt='python' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>Python</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/typescript.png' alt='typescript' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>TypeScript</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/git.png' alt='git' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>Git</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/html.png' alt='html' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>HTML</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/css.png' alt='css' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>CSS</span>
+              </div>
+            </div>
+            <h3>Frameworks y Librerias</h3>
+            <div className='flex mt-2 space-x-2'>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/react.png' alt='react' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>React</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/react.png' alt='react native' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>React Native</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/nodejs.png' alt='nodejs' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>Node.JS</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/bootstrap.png' alt='bootstrap' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>BootStrap</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/tailwind.png' alt='tailwind' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>Tailwind</span>
+              </div>
+            </div>
+            <h3>Base de Datos</h3>
+            <div className='flex mt-2 space-x-2'>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/mongodb.png' alt='mongodb' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>MongoDB</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/postgresql.png' alt='postgresql' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>PostgreSQL</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/mysql.png' alt='mysql' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>MYSQL</span>
+              </div>
+            </div>
+            <h3>Herramientas de Desarrollo y Colaboracion</h3>
+            <div className='flex mt-2 space-x-2'>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/github2.png' alt='github2' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>GitHub</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/figma.png' alt='figma' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>Figma</span>
+              </div>
+              <div className='flex flex-col items-center'>
+                <img src='img/icons/docker.png' alt='docker' className='w-10 h-10'/>
+                <span className='text-xs text-gray-500'>Docker</span>
+              </div>
+            </div>
+          </div>
+        </div>
   )
 }
 export default Technologies
--- src/utils/PDFViewer.tsx
+++ src/utils/PDFViewer.tsx
@@ -1,29 +1,58 @@
-import React, { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
-import pdf from './CV- Christian Taboada FullStack.pdf';
+import React, { useState } from 'react'
+import { Document, Page, pdfjs } from 'react-pdf'
+import { IconContext } from 'react-icons'
+import { CgArrowLongLeft, CgArrowLongRight } from "react-icons/cg";
+
+import pdf from './CV- Christian Taboada FullStack.pdf'
 
 // Configuración del trabajador de PDF.js
-pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
 
 const PDFViewer: React.FC = (): JSX.Element => {
-  const [numPages, setNumPages] = useState<number>();
-  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [numPages, setNumPages] = useState<number>()
+  const [pageNumber, setPageNumber] = useState<number>(1)
 
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages);
+  function onDocumentLoadSuccess ({ numPages }: { numPages: number }): void {
+    setNumPages(numPages)
+  }
+
+  const goToPreviousPage = (): void => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1)
+    }
+  }
+
+  const goToNextPage = (): void => {
+    if (pageNumber < numPages) {
+      setPageNumber(pageNumber + 1)
+    }
   }
 
   return (
     <div>
+      <div className='flex align-center justify-center space-x-5 bg-slate-600 text-white'>
+        <button onClick={goToPreviousPage} disabled={pageNumber <= 1}>
+          <IconContext.Provider value={{ className: 'text-white w-8 h-8' }}>
+            <CgArrowLongLeft/>
+          </IconContext.Provider>
+        </button>
+        <span>
+          Page {pageNumber} of {numPages}
+        </span>
+        <button onClick={goToNextPage} disabled={pageNumber >= numPages}>
+        <IconContext.Provider value={{ className: 'text-white w-8 h-8' }}>
+            <CgArrowLongRight/>
+          </IconContext.Provider>
+        </button>
+      </div>
       <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
+        <Page pageNumber={pageNumber}
+        renderTextLayer={false}
+        renderAnnotationLayer={false}
+        customTextRenderer={false}/>
       </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
     </div>
-  );
-};
+  )
+}
 
-export default PDFViewer;
-
+export default PDFViewer
